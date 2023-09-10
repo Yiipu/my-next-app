270484cb1845b773925d776ee1813c7eaa7ebbb4
Yiipu <jaap20035@outlook.com>
2023-09-10 17:16:43 +0800

    编写了main部分

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
-import Banner from '@/components/banner';
+import Banner from "@/components/banner"
+
+// 接口
+const articles = [
+  { key: 1, href: "/", title: "Hello", description: "Hello ?" },
+  { key: 2, href: "/", title: "About", description: "Hello !" },
+  { key: 3, href: "/", title: "Setting", description: "Hello ." },
+]
 
 export default function Home() {
   return (
-    <div>
-      <Banner></Banner>
-    </div>
+    <>
+    <Banner />
+    <ul className="flex flex-wrap mt-2">
+      {articles.map(
+        (a) => {
+          return (
+            <li className="mb-2 w-full md:w-2/4 px-2"><a href={a.href}>
+              <div className="card bg-perfume-600 text-white">
+                <h3>{a.title}</h3>
+                <p>{a.description}</p>
+              </div>
+            </a></li>
+          )
+        })}
+    </ul>
+    </>
+
   )
 }
--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -3,7 +3,7 @@ import bannerPic from '@/media/images/banner.png'
 export default function Banner() {
     return (
         <div className="w-full mx-auto md:max-w-md lg:max-w-lg xl:max-w-xl xxl:max-w-xxl">
-            <div className="relative h-96 bg-cover bg-center" style={{backgroundImage: `url(${bannerPic.src})`}}>
+            <div className="h-96 bg-cover bg-center rounded-xl mx-2" style={{backgroundImage: `url(${bannerPic.src})`}}>
                 <div className="w-2/4 p-8 flex flex-col h-full justify-evenly">
                     <h2>Hello World</h2>
                     <p>just doing something...</p>
